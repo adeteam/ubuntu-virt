01a46ef2c5bb9a40a88850077c5080f6643b0507
root <root@ubuntu.vm>
2019-08-07 19:27:11 +0000

    delay vmware installation till startup

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,9 @@
 # install build utils (NOTE: import that this is install during container startup and not during build)
 apt-get install -y linux-headers-$(uname -r) linux-image-$(uname -r)
 
+# install the vmware workstation
+sudo ./VMware-Workstation-Full-14.0.0-6661328.x86_64.bundle --eulas-agreed --console --required
+
 # configure the vmware modules
 /usr/bin/vmware-modconfig --console --install-all || true
 
