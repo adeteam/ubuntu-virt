fdc52e9f582e66b1ca6d5c742f6598e9273e45e9
root <root@ubuntu.vm>
2019-08-07 20:03:44 +0000

    allow ovftool 4.3.0 install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,9 @@ apt-get install -y linux-headers-$(uname -r) linux-image-$(uname -r)
 # install the vmware workstation
 sudo /opt/vmware/VMware-Workstation-Full-14.0.0-6661328.x86_64.bundle --eulas-agreed --console --required
 
+# install the vmware ovftool
+sudo /opt/vmware/VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle --eulas-agreed --console --required
+
 # configure the vmware modules
 /usr/bin/vmware-modconfig --console --install-all || true
 
